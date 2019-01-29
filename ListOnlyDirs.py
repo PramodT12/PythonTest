d1eb7c5eb0514d34745fdbb77693e645fce77188
Pramod Kumar Tiwari <PTiwari9@slb.com>
2019-01-29 15:18:53 +0530

    Updated

--- ListOnlyDirs.py
+++ ListOnlyDirs.py
@@ -1,12 +1,11 @@
 #List only Directories
 import os
-TxtL=[]
-PyL=[]
-FDList=os.listdir()
+DirL=[]
+FDList=os.listdir("C:\\")
+print(FDList)
 for i in FDList:
-    if ".txt" in i:
-        TxtL.append(i)
-    elif ".py" in i:
-        PyL.append(i)
-print("Txt Files :",TxtL)
-print("Python files :",PyL)
+    i='C:\\'+i
+    if os.path.isdir(i):
+        DirL.append(i)
+print("DIrectories List :",DirL)
+
