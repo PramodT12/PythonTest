a11ce371a7cb8dd427eddae476603b33df338488
Pramod Kumar Tiwari <PTiwari9@slb.com>
2019-01-30 10:44:25 +0530

    Updated set

---
+++ CmdLineArg.py
@@ -0,0 +1,6 @@
+import sys
+
+print(sys.argv)
+print("Filename :",sys.argv[0])
+print("First Argument :",sys.argv[1])
+print("Second Argument :",sys.argv[2])
---
+++ FileWrite.py
@@ -0,0 +1,7 @@
+#File writing
+fobj=open("Testfile.txt","w")
+fobj.write("Welcome to the session\n")
+fobj.write("Python\n")
+fobj.write("Perl\n")
+fobj.write("100")
+fobj.close()
